26a7221f3284daa411550ecc05563212f1d3731c
Nathan Leigh <nleigh11@hotmail.com>
2016-01-11 16:41:21 +0000

    website .net

---
+++ NathanLeigh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NathanLeigh.MvcApplication" Language="C#" %>
---
+++ NathanLeigh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NathanLeigh.Startup))]
+namespace NathanLeigh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
